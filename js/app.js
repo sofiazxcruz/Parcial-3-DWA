472749d30c3816a234788b5338f5f22432479c7a
Denisexper <clasic.exper2023@gmail.com>
2024-12-09 23:09:48 -0600

    feat: creating js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+/* function wacht(button) {
+    
+    const article = button.closest('.project-item');
+    
+    const description = article.querySelector('.description');
+
+    // Alternar la visibilidad de la descripción
+    if (description.style.display === "none") {
+        description.style.display = "block";
+        button.textContent = "Hide Description";
+    } else {
+        description.style.display = "none";
+        button.textContent = "Description";
+    }
+} */
